7abcf7f8b7783bb6ec352bb5aa2efd6c21b0fd41
yasuro <voronov.maksim.v@gmail.com>
2021-03-31 00:16:18 +0300

    +

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,42 @@
 <template>
     <v-app id="page">
 
-        <v-app-bar id="app-bar">
-            <v-row class="white--text">
-                <v-col>
+        <v-app-bar id="app-bar" app>
+            <v-row class="white--text text-center">
+                <v-app-bar-title class="ma-5">
                     <v-row no-gutters class="flex-column text-center">
                         <v-col style="color: red">K&K</v-col>
-                        <v-col>Киселев & КОмпания</v-col>
+                        <v-col>Киселев & Компания</v-col>
                     </v-row>
-                </v-col>
-
-                <v-col>
+                </v-app-bar-title>
+                <v-col align-self="center">
                     Главная
                 </v-col>
 
-                <v-col>
+                <v-col align-self="center">
                     Портфолио
                 </v-col>
-                <v-col>
+                <v-col align-self="center">
                     Контакты
                 </v-col>
             </v-row>
         </v-app-bar>
 
         <v-main app id="app-content">
-            <v-container >
+            <v-container>
                 <vor-home></vor-home>
             </v-container>
         </v-main>
+
+        <v-footer app fixed id="app-footer" elevation="12">
+            <v-row class="text-center white--text">
+                <v-col>
+                    Это футер
+                </v-col>
+            </v-row>
+        </v-footer>
+
+
     </v-app>
 </template>
 
@@ -44,10 +53,13 @@
 
 <style>
 
+    html{
+        overflow-y:auto; /*ЧТобы скрыть полузнок прокрутки*/
+    }
 
     #app-bar {
         background: #272727;
-        flex: unset;
+
     }
 
     #app-footer {
