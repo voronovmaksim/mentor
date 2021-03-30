6c065aacf9fc1a7660ec44b7411bbb8f894d0b4a
yasuro <voronov.maksim.v@gmail.com>
2021-03-30 23:02:53 +0300

    Final layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
     <v-app id="page">
-        <v-main>
-            <v-container>
+
+        <v-app-bar id="app-bar">
+            <v-row class="white--text">
+                <v-col>
+                    <v-row no-gutters class="flex-column text-center">
+                        <v-col style="color: red">K&K</v-col>
+                        <v-col>Киселев & КОмпания</v-col>
+                    </v-row>
+                </v-col>
+
+                <v-col>
+                    Главная
+                </v-col>
+
+                <v-col>
+                    Портфолио
+                </v-col>
+                <v-col>
+                    Контакты
+                </v-col>
+            </v-row>
+        </v-app-bar>
+
+        <v-main app id="app-content">
+            <v-container >
                 <vor-home></vor-home>
             </v-container>
         </v-main>
@@ -19,3 +42,21 @@
     }
 </script>
 
+<style>
+
+
+    #app-bar {
+        background: #272727;
+        flex: unset;
+    }
+
+    #app-footer {
+        background: #272727;
+    }
+
+    #app-content {
+        background: #272727;
+    }
+
+
+</style>
